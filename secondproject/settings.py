43043384b49103b306056974daf9dfcb3f2c86fe
bubssi528 <intel1567@gmail.com>
2019-07-31 18:46:26 +0900

    sdfd

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-        #allauth
+    #allauth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
