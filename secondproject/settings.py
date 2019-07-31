ee2cd62acfe6d4916b3d531423a4815eec3caf07
bubssi528 <intel1567@gmail.com>
2019-07-31 12:38:41 +0900

    upgrade django

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')   #'$1@evx&u_4f%%ub+@r=9#o5bb^@(kb=#xxoh)%kmg=d=h@7j-t'
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) ) #True
